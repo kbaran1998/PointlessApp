b6e05a2e69ca1344acefd6c71f76d72f99e34415
Krzysztof <krysbaran@gmail.com>
2019-07-28 20:55:02 +0200

    Add QuotesList class

--- app/src/main/java/com/example/pointless/QuoteList.java
+++ app/src/main/java/com/example/pointless/QuoteList.java
@@ -1,5 +1,41 @@
 package com.example.pointless;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Stack;
+
+
 public class QuoteList {
+    private Stack<String> quotes;
+    private List<String> usedQuotes;
+    private String currentQuote;
+
+    public QuoteList(Stack<String> quotes) {
+        assert !quotes.empty();
+        this.quotes = quotes;
+        usedQuotes = new ArrayList<>();
+        currentQuote =  quotes.pop();
+        usedQuotes.add(currentQuote);
+    }
+
+    public String getCurrentQuote() {
+        return currentQuote;
+    }
+
+    public void setCurrentQuote() {
+        if (quotes.empty()) {
+            Collections.shuffle(usedQuotes);
+            int originalSize = usedQuotes.size();
+            for (int i = 0; i < originalSize; i++) {
+                quotes.push(usedQuotes.get(0));
+                usedQuotes.remove(0);
+            }
+        }
+
+        currentQuote = quotes.pop();
+        usedQuotes.add(currentQuote);
+    }
 
 }
