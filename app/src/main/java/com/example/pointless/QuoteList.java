be0594423aaf3ebf4bd777059e7a3f8bb12e2dd0
Krzysztof <krysbaran@gmail.com>
2019-08-04 17:56:21 +0200

    Add comments and improve code style

--- app/src/main/java/com/example/pointless/QuoteList.java
+++ app/src/main/java/com/example/pointless/QuoteList.java
@@ -1,38 +1,59 @@
 package com.example.pointless;
 
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
+import java.util.LinkedList;
 import java.util.Stack;
 
-
+/**
+ * A data structure of quotes that will be used in the application. The QuoteList has a stack
+ * of quotes, like a stack of cards, where you pop the quote that is on top of the stack and
+ * place it in a LinkedList of used quotes which is shuffled after the stack is empty and put
+ * back to the stack. That means that the likelihood of getting the same quote twice is small.
+ * You can only get current quote.
+ *
+ * @author Krzysztof Baran
+ */
 public class QuoteList {
-    private Stack<String> quotes;
-    private List<String> usedQuotes;
+    private final static String EMPTY_QUOTE_STACK = "Hello There!";
+    private Stack<String> quotesStack;
+    private LinkedList<String> usedQuotes;
     private String currentQuote;
 
+    /**
+     * A constructor for Quote.
+     * @param quotes stack with quotes
+     */
     public QuoteList(Stack<String> quotes) {
-        this.quotes = quotes;
-        usedQuotes = new ArrayList<>();
+        this.quotesStack = quotes;
+        if (this.quotesStack.isEmpty()) {
+            quotes.push(EMPTY_QUOTE_STACK);
+        }
+        usedQuotes = new LinkedList<>();
         currentQuote =  quotes.pop();
         usedQuotes.add(currentQuote);
     }
 
+    /**
+     * Getter for the current quote in the stack.
+     * @return current Quote
+     */
     public String getCurrentQuote() {
         return currentQuote;
     }
 
-    public void setCurrentQuote() {
-        if (quotes.empty()) {
+    /**
+     * Setting the next quote to use. Once the stack is empty, list of used quotes is shuffled and
+     * placed back to the stack.
+     */
+    public void nextQuote() {
+        if (quotesStack.empty()) {
             Collections.shuffle(usedQuotes);
-            int originalSize = usedQuotes.size();
-            for (int i = 0; i < originalSize; i++) {
-                quotes.push(usedQuotes.get(0));
-                usedQuotes.remove(0);
+            while(!usedQuotes.isEmpty()) {
+                quotesStack.push(usedQuotes.poll());
             }
         }
 
-        currentQuote = quotes.pop();
+        currentQuote = quotesStack.pop();
         usedQuotes.add(currentQuote);
     }
 
--- app/src/test/java/com/example/pointless/QuoteListTest.java
+++ app/src/test/java/com/example/pointless/QuoteListTest.java
@@ -11,42 +11,67 @@ import java.util.Stack;
 
 import static org.junit.Assert.assertEquals;
 
+/**
+ * Test for QuotesList data structure.
+ */
 public class QuoteListTest {
     private QuoteList quoteList;
     private Stack<String> quoteStack;
 
+    /**
+     * Stack initializer.
+     */
     @BeforeEach
-    public void before() {
+    void before() {
         quoteStack = new Stack<>();
     }
 
+    /**
+     * Test adding only one quote to the list and see what is the current quote.
+     * @param testString string that will be used for that test
+     */
     @ParameterizedTest
     @ValueSource(strings = {"Text1", "Text2", "Text3"})
-    public void getCurrentQuoteWhenCreatedTest(String testString) {
+    void getCurrentQuoteWhenCreatedTest(String testString) {
          quoteStack.push(testString);
          quoteList = new QuoteList(quoteStack);
          assertEquals(quoteList.getCurrentQuote(), testString);
     }
 
+    /**
+     * Test to check that when having multiple elements, the top element of the stack is picked.
+     * @param strList string that contains substrings of quotes
+     * @param correct the result that should be outputted.
+     */
     @ParameterizedTest
-    @CsvSource({"Text1 Text2 Text3, Text3", "Text3 Text2, Text2", "Text2, Text2"})
-    public void getCurrentQuoteWhenCreatedTestMultiple(String strList, String correct) {
+    @CsvSource({"Text1_Text2_Text3, Text3", "Text3_Text2, Text2", "Text2, Text2",
+            "Text1 and Text2_Text3, Text3"})
+    void getCurrentQuoteWhenCreatedTestMultiple(String strList, String correct) {
         strListPlacedToStack(strList);
         quoteList = new QuoteList(quoteStack);
         assertEquals(quoteList.getCurrentQuote(), correct);
     }
 
+    /**
+     * Test to check that getting next quote gets what was below the stack.
+     */
     @Test
-    public void changeQuoteToNext() {
+    void changeQuoteToNext() {
         quoteStack.push("Text3");
         quoteStack.push("Text2");
         quoteList = new QuoteList(quoteStack);
-        quoteList.setCurrentQuote();
+        quoteList.nextQuote();
         assertEquals(quoteList.getCurrentQuote(), "Text3");
     }
 
-    public void strListPlacedToStack(String str) {
+    /**
+     * Method to parse substrings of the quotes into individual strings.
+     * @param str long string with substrings.
+     */
+    void strListPlacedToStack(String str) {
+        String delimiter = "_";
         Scanner scanner = new Scanner(str);
+        scanner.useDelimiter(delimiter);
         while(scanner.hasNext()) {
             quoteStack.push(scanner.next());
         }
